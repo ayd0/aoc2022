84164aba9a71476fcd862012577b1eb7b47529fe
ayd0 <austindaytonogle@gmail.com>
2023-11-13 22:19:26 -0800

    fixed path error, updated cd and dir cmd handling

--- day7/addFS.js
+++ day7/addFS.js
@@ -7,7 +7,7 @@ if (process.argv[3] == 1) {
     let numDirs = 0;
 
     data.forEach(el => {
-        if (+el[0] > 0 || +el[0] < 0) {
+        if (+el[0] > 0) {
             // only strings which typecast to real numbers can be > 0 || < 0
             numFiles++;
         } else if (el[0] === 'd') {
@@ -25,18 +25,21 @@ if (process.argv[3] == 1) {
         * 2 - subDirs
         *   *- if subDirs is empty, path should be 0!
         */
-        const dir = ['/', 0, []];
+
+    const dir = ['/', 0, []];
     let path = [0];
 
-    const getCurDir = (ovPath) => {
+    const getCurDir = (ovPath, total) => {
         // override path
         if (ovPath === undefined) ovPath = path;
-        console.log(Array.isArray(ovPath), ovPath);
         let pseudo = dir;
 
-        ovPath.forEach(seq => {
-            // should only be selecting index 0 when home is empty
+        ovPath.forEach((seq, idx) => {
+            // arg for incrementFileSizes(), adds total up stack to fs
+            if (total !== undefined && idx !== path.length) pseudo[1] += total;
+
             if (seq > 0) {
+                // should only be selecting index 0 when home is empty
                 pseudo = pseudo[2][seq - 1];
             } else {
                 return pseudo;
@@ -47,10 +50,8 @@ if (process.argv[3] == 1) {
     }
 
     const getLastDirName = () => {
-        if (getCurDir[0] !== '/') {
-            // Path ERROR is HERE
+        if (getCurDir()[0] !== '/') {
             let ovPath = path.slice().pop();
-
             return getCurDir(ovPath)[0];
         } else {
             return '/';
@@ -61,10 +62,29 @@ if (process.argv[3] == 1) {
         getCurDir()[2].push([getLastDirName() + name, 0, []]);
     }
 
+    const incrementFileSizes = () => {
+        // in FIRST pass, only write to getCurDir[1] if it's EQUAL to 0 
+        // similarly, only write up stack when getCurDir[1] is EQUAL to 0
+        
+        let total = 0;
+        if (getCurDir()[1] === 0) { 
+            if (getCurDir().length > 3) { 
+                getCurDir().slice(3).forEach(num => total += num);
+                getCurDir()[1] = total;
+            }
+            // TODO: increment total up stack
+            getCurDir(undefined, total)
+        }
+    }
+
     data.some(cmd => {
         try {
             if (cmd[0] === '$') {
                 if (cmd[2] === 'c') {
+                    // handle file sizes on directory change
+                    incrementFileSizes();
+
+                    // process cd command
                     if (cmd[5] === '/') {
                         // rooot
                         path = [0];
@@ -74,7 +94,17 @@ if (process.argv[3] == 1) {
                     } else {
                         // down: name
                         const dirName = cmd.slice(5);
-                        console.log('GRABBIN DIR NAME', dirName);
+                        let idx;
+                        // [].indexOf not working in this case, hacky implementation
+                        getCurDir()[2].some((el, i) => {
+                            if (el[0].slice(el[0].length - dirName.length) === dirName) {
+                                idx = i;
+                                return true;
+                            }
+                            return false;
+                        });
+                        path[path.length - 1] += idx + 1;
+                        path.push(0);
                     }
                 }
             } else if (cmd[0] === 'd') {
@@ -88,7 +118,7 @@ if (process.argv[3] == 1) {
                 }
             } else if (+cmd[0] >= 0) {
                 // cmd[0] is real num
-                // ONLY WRITE TO getCurDir[1] IF IT'S EQUAL TO 0
+                getCurDir().push(Number(cmd.match(/\d+/)));
             }
         } catch (err) {
             console.error(err);
@@ -99,6 +129,9 @@ if (process.argv[3] == 1) {
         }
         return false;
     })
+    // last pass w/o cd
+    incrementFileSizes();
+
     console.log("DIR", dir);
     console.log("PATH", path);
 }
